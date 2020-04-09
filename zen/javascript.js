b9b02c03e721a154187a343a59cdf418a678fe16
tabris <orange.sheep@gmail.com>
2020-04-08 17:13:05 -0700

    updating to new koi

--- zen/javascript.js
+++ zen/javascript.js
@@ -1,15 +1,20 @@
 let quotes = [
 "When you realize nothing is lacking, the whole world belongs to you. - Lao Tzu",
-"What the superior man seeks is in himself; what the small man seeks is in others. - Confucius",
+"What the superior man seeks is in himself; what the small man seeks is in others.  - Confucius",
 "To seek is to suffer. To seek nothing is bliss.",
-"I live by letting things happen. Dogen",
-"Who you are is always right. Ming-Dao Deng"
+"I live by letting things happen. - Dogen",
+"Who you are is always right. - Ming-Dao Deng"
 ]
 
-window.addEventListener('load',newQuote);
+//window.addEventListener('load',newQuote);
+
+
 
 function newQuote() {
-  let randomNumber = Math.floor(Math.random() * (quotes.length));
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  var container = document.getElementById('container');
   document.getElementById('quote').innerHTML = quotes[randomNumber];
-
+  var ripple = document.createElement('div');
+  ripple.setAttribute("class","ripple");
+  container.appendChild(ripple);
 }
